1dac41da2df782dc4aae17020e950d78f8224d3e
yourhappiness <tasya32@yandex.ru>
2020-08-16 17:55:47 +0300

    init

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Anastasia Romanova on 16.08.2020.
+//  Copyright © 2020 Anastasia Romanova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
